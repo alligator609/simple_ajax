e5fd32e2f7ac0641be98e2994d7fa370ba3619c1
Zia Uddin Ahmed <zia.ete10@gmail.com>
2021-02-20 01:40:40 +0600

    added handlebars js to the existing code

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var animalContainer = document.getElementById("animal-info");
 btn.addEventListener("click",function(){
 
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-'+ pageCounter +'.json');
+    ourRequest.open('GET','https://learnwebcode.github.io/json-example/pets-data.json');
     ourRequest.onload =function() {
         if (ourRequest.status >= 200 && ourRequest.status < 400) {
             var ourData = JSON.parse(ourRequest.responseText);
@@ -22,35 +22,27 @@ btn.addEventListener("click",function(){
     }
 });
 
+Handlebars.registerHelper("calculateAge", function(birthYear) {
+    var age = new Date().getFullYear() - birthYear;
+  
+    if (age > 0) {
+      return age + " years old";
+    } else {
+      return "Less than a year old";
+    }
+  
+  });
 
 function renderHtml(data){
-    var htmlString = "";
 
-    for (var i= 0; i< data.length; i++){
-        htmlString +="<p>" +data[i].name + " is a " + data[i].species + " that likes to eat ";
+    var rawTempate = document.getElementById("petsTemplate").innerHTML;
+    var compiledTemplate = Handlebars.compile(rawTempate);
+    var ourGeneratedHTML = compiledTemplate(data);
+
+    animalContainer.innerHTML = ourGeneratedHTML;
     
-        for (ii = 0; ii < data[i].foods.likes.length; ii++) {
-          if (ii == 0) {
-            htmlString += data[i].foods.likes[ii];
-          } else {
-            htmlString += " and " + data[i].foods.likes[ii];
-          }
-        }
-    
-        htmlString += ' and dislikes ';
-    
-        for (ii = 0; ii < data[i].foods.dislikes.length; ii++) {
-          if (ii == 0) {
-            htmlString += data[i].foods.dislikes[ii];
-          } else {
-            htmlString += " and " + data[i].foods.dislikes[ii];
-          }
-        }
-    
-        htmlString += '.</p>';
-    
-      }
-    animalContainer.insertAdjacentHTML('beforeend',htmlString);
 }
+
+
 
 
